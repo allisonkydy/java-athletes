afdc075b1391fa1ddb41a9b75399656931a9f443
allisonkydy <allisnelly@gmail.com>
2019-10-03 15:47:05 -0500

    add interfaces, constructor injection in app

---
+++ athlete/src/com/lambdaschool/solution/App.java
@@ -0,0 +1,14 @@
+package com.lambdaschool.solution;
+
+public class App
+{
+  // constructor injection
+  private AthleteCreation athleteCreation;
+
+  public App(AthleteCreation athleteCreation)
+  {
+    this.athleteCreation = athleteCreation;
+  }
+
+
+}
